06293c809b50a4ec04c61a2909adc7609cfaa45b
Annick Cang <annick.cang@gmail.com>
2026-05-03 22:52:39 +0200

    docs(data): update normalization function docstring

--- donnees.py
+++ donnees.py
@@ -5,7 +5,7 @@ import types_structure
 import constantes as const
 
 def normaliser_chaine_pour_comparaison(chaine: str)-> str:
-    """Renvoie une chaîne sans les accents pour le tri alphabétique"""
+    """Renvoie une chaîne en minuscule et sans les accents"""
     chaine_retour = unicodedata.normalize('NFD', chaine.lower())
     chaine_retour = ''.join(c for c in chaine_retour
                             if unicodedata.category(c) != 'Mn')
