f36c61bcbc3e9a58d282c3a4aac848793fe7628c
Annick Cang <annick.cang@gmail.com>
2026-09-09 21:52:57 +0200

    test(stock): add test for ajouter_produit function

--- tests/test_gestion_stock.py
+++ tests/test_gestion_stock.py
@@ -26,3 +26,23 @@ def test_trouver_produit(nom_recherche, produit_attendu):
     resultat = gestion_stock.trouver_produit(stock, nom_recherche)
 
     assert resultat == produit_attendu
+
+
+def test_ajouter_produit():
+    stock: list[types_structure.Produit] = [
+        {"nom": "bonbon", "quantite": 15, "seuil": 5, "prix": 0.5},
+        {"nom": "Café", "quantite": 3, "seuil": 2, "prix": 18.5},
+    ]
+    stock_attendu: list[types_structure.Produit] = [
+        {"nom": "bonbon", "quantite": 15, "seuil": 5, "prix": 0.5},
+        {"nom": "Café", "quantite": 3, "seuil": 2, "prix": 18.5},
+        {"nom": "CHOCOLAT", "quantite": 1, "seuil": 2, "prix": 3.75},
+    ]
+    nom = "CHOCOLAT"
+    quantite = 1
+    seuil = 2
+    prix = 3.749
+
+    gestion_stock.ajouter_produit(stock, nom, quantite, seuil, prix)
+
+    assert stock == stock_attendu
