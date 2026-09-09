5e5326572d14d932518dd35fcbdb2853d43a61f8
Annick Cang <annick.cang@gmail.com>
2026-09-09 21:16:07 +0200

    test(stock): add tests for trouver_produit function

---
+++ tests/test_gestion_stock.py
@@ -0,0 +1,28 @@
+import pytest
+
+import gestion_stock
+import types_structure
+
+
+@pytest.mark.parametrize(
+    ("nom_recherche", "produit_attendu"),
+    [
+        ("banane", None),
+        ("café", {"nom": "Café", "quantite": 3, "seuil": 2, "prix": 18.5}),
+        ("cafe", {"nom": "Café", "quantite": 3, "seuil": 2, "prix": 18.5}),
+    ],
+)
+
+
+def test_trouver_produit(nom_recherche, produit_attendu):
+    stock: list[types_structure.Produit] = [
+        {"nom": "bonbon", "quantite": 15, "seuil": 5, "prix": 0.5},
+        {"nom": "Café", "quantite": 3, "seuil": 2, "prix": 18.5},
+        {"nom": "CHOCOLAT", "quantite": 1, "seuil": 2, "prix": 3.75},
+        {"nom": "M&M's", "quantite": 10, "seuil": 3, "prix": 2.5},
+        {"nom": "Tomate cerise", "quantite": 5, "seuil": 10, "prix": 0.75},
+    ]
+
+    resultat = gestion_stock.trouver_produit(stock, nom_recherche)
+
+    assert resultat == produit_attendu
